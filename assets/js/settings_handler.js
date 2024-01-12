0c054c19003ce1d5aa27594fdc7f1e09e5e56cc8
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-01-12 02:46:21 +0000

    feat: Add validateTeams function to check team setup and handle redirection.

--- assets/js/settings_handler.js
+++ assets/js/settings_handler.js
@@ -16,7 +16,31 @@ function setValueAndProgress(input) {
 }
 
 
+/**
+ * Checks if teams are set up. If not, prompts user and redirects based on user's choice.
+ */
+function validateTeams() {
+    // Retrieve teams from local storage
+    const teams = JSON.parse(localStorage.getItem("gameOptions")).teams;
+
+    // Validate teams
+    if (!Array.isArray(teams) ||  teams.length < 2) {
+        // Prompt user
+        let userConfirmation = confirm("No teams set up. Click OK to set up teams, or Cancel to return to the main page.");
+
+        // Redirect based on user's choice
+        if (userConfirmation) {
+            window.location.href = "./teams.html";
+        } else {
+            window.location.href = "./index.html";
+        }
+    }
+}
+
+
 window.addEventListener("DOMContentLoaded", function () {
+    validateTeams();
+
     const rangeInput = document.querySelectorAll('.range-input');
 
     for (let input of rangeInput) {
