ece1ed9e9d1fa6d2ac3e3a21acebf15a71a363d6
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-01-29 15:44:57 +0000

    refactor: Move the "createWordList()" func from "game_handler.js" to "categories_handler.js"

--- assets/js/categories_handler.js
+++ assets/js/categories_handler.js
@@ -1,3 +1,46 @@
+import { getGameOptions, setGameOptions } from "./script.js";
+
+/**
+ * Create a word list based on the game type
+ */
+function createWordList() {
+    const gameOptions = getGameOptions();
+    let path;
+
+    // Set path to the JSON file based on the game type
+    if (gameOptions.type === "easy") {
+        path = "./easy_words.json";
+    } else if (gameOptions.type === "medium") {
+        path = "./medium_words.json";
+    } else {
+        path = "./hard_words.json";
+    }
+
+    // Fetch the JSON file and save it to the gameOptions object
+    fetch(path)
+        .then(response => {
+            // Check if the response is OK
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Save the words to the gameOptions object
+            gameOptions.words = data;
+            setGameOptions(gameOptions);
+            
+            // Redirect to the game page
+            window.location.href = "./game.html";
+
+        })
+        .catch(error => {
+            // If there is an error, disable the page and show an alert
+            document.querySelector("body").classList.add("disabled");
+            alert("Something went wrong. Please reload the page.")
+        });
+}
+
 /**
  * Set type for the game
  */
@@ -14,7 +57,7 @@ function setType(event) {
     }
 
     localStorage.setItem("gameOptions", JSON.stringify(gameOption));
-    window.location.href = "./game.html";
+    createWordList();
 }
 
 
